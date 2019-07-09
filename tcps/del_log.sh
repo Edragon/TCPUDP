d90979ece2407a107e0429696bfabd195cb6c50e
hechao <root@iot2.electrodragon.com>
2019-07-09 03:33:12 -0400

    tcps

--- tcps/del_log.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pkill -f "/var/www/html/tcp/main.py"
-
-rm -rf /var/www/html/tcp/index.html
-
-nohup python ~/TCPUDP/tcp_server_1/main.py &
--- tcps/main.py
+++ tcps/main.py
@@ -28,12 +28,24 @@ def on_new_client(clientsocket, addr):
     while True:
         data = clientsocket.recv(182).encode('hex') #182
 
-        print codec_data
+        print data
 		
-        log.warning('Received %r from %s <br>' % (data, addr))
+	if data[0:4] == '7e7e':
+		#print ("ED20 GPS Tracker: star_char-%s, version-%s, action-%s, pack_times-%s" % (data[0:4], data[4:12], data[12:16], data[16:20]) ) # simple print demo
+		log.warning('ED20 GPS Tracker Brief: star_char-%s, version-%s, action-%s, pack_times-%s  <br>' % (data[0:4], data[4:12], data[12:16], data[16:20])   )
 		
-        clientsocket.send("Received at Electrodragon tracker")
-        
+		log.warning('ED20 GPS Tracker SN: Time-%s, ID-%s, SN-%s  <br>' % (data[20:32], data[74:86], data[44:56])   )
+		
+		log.warning('ED20 GPS Tracker GPS Data: type- %s, :LOG-%s, LAT-%s  <br>' % (data[106:114], data[114:122], data[122:130])   )
+		
+		log.warning('ED20 GPS Tracker Full Data %r from %s <br>' % (data, addr))
+		
+		clientsocket.send("Received at Electrodragon tracker")
+		
+        else:
+	    	log.warning('Received TCP Random Data %r from %s <br>' % (data, addr))
+		
+		
     clientsocket.close()
 
 
