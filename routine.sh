73523fa27cb9089d9263d5fd7659d1559353c127
hechao <root@iot2.electrodragon.com>
2019-07-09 03:51:17 -0400

    update routine

--- routine.sh
+++ routine.sh
@@ -4,3 +4,10 @@ pkill -f "/root/TCPUDP/tcps/main.py"
 rm -rf /var/www/html/tcp/index.html
 
 nohup python /root/TCPUDP/tcps/main.py &
+
+
+pkill -f "/root/TCPUDP/udps/main.py"
+
+rm -rf /var/www/html/udp/index.html
+
+nohup python /root/TCPUDP/udps/main.py &
